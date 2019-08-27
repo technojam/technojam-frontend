becf6e16efed7806ac9226fc6430ce35f92e9781
ravi nandan saxena <44205807+ravisaxena23@users.noreply.github.com>
2019-08-28 00:55:42 +0530

    initial

---
+++ technojam/src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+
+const NavBar = () => {
+return(
+  <div>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="title" color="inherit">
+            TechnoJam
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  </div>
+)
+
+}
+
+export default NavBar;
