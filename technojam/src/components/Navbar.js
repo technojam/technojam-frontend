2a6e62d3066d779c27d5f3cf0e1fce4b84218338
ravi nandan saxena <44205807+ravisaxena23@users.noreply.github.com>
2019-08-28 01:42:51 +0530

    initial

--- technojam/src/components/Navbar.js
+++ technojam/src/components/Navbar.js
@@ -2,6 +2,25 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+
+const styles = {
+  // these buttons will be aligned to right side of abbBar
+  toolbarButtons: {
+    marginLeft: "auto",
+    marginRight: -12
+  },
+  menuButton: {
+    marginRight: 20,
+    marginLeft: -12
+  }
+};
 
 
 const NavBar = () => {
@@ -9,6 +28,9 @@ return(
   <div>
     <AppBar position="static">
       <Toolbar>
+      <IconButton edge="start" color="inherit" className="classes.menuButton" aria-label="menu" position="middle">
+            <MenuIcon />
+      </IconButton>
         <Typography variant="title" color="inherit">
             TechnoJam
         </Typography>
