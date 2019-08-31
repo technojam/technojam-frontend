69778dd599c6455587bcf2e292d55de72c655da1
ravi nandan saxena <44205807+ravisaxena23@users.noreply.github.com>
2019-09-01 02:57:55 +0530

    footer ready

--- src/Components/Layouts/Footer.js
+++ src/Components/Layouts/Footer.js
@@ -25,10 +25,10 @@ export default props =>
                 </Grid>
                 <Grid container direction="row" >
                     <div style={{ paddingLeft:'200px' }} >
-                        <a className="btn btn-social-icon btn-twitter" href=" https://twitter.com/technojam_gu"><i className="fab fa-twitter fa-lg"></i></a>
-                        <a className="btn btn-social-icon btn-facebook" href="facebook.com/technojam.scse "><i className="fab fa-facebook fa-lg"></i></a>
-                        <a className="btn btn-social-icon btn-instagram" href="https://instagram.com/technojam_galgotias"><i className="fab fa-instagram fa-lg"></i></a>
-                        <a className="btn btn-social-icon btn-linkedin" href="https://www.linkedin.com/company/technojam/"><i className="fab fa-linkedin fa-lg"></i></a>
+                        <a className="btn btn-social-icon btn-twitter" href=" https://twitter.com/technojam_gu"><i className="fab fa-twitter fa-lg"style={{color:'#28aae1'}}></i></a>
+                        <a className="btn btn-social-icon btn-facebook" href="facebook.com/technojam.scse "><i className="fab fa-facebook fa-lg"style={{color:'#3b5998'}}></i></a>
+                        <a className="btn btn-social-icon btn-instagram" href="https://instagram.com/technojam_galgotias"><i className="fab fa-instagram fa-lg"style={{color:'#e41a2b'}}></i></a>
+                        <a className="btn btn-social-icon btn-linkedin" href="https://www.linkedin.com/company/technojam/"><i className="fab fa-linkedin fa-lg" style={{color:'#0077b5'}}></i></a>
                         
                     </div>
                 </Grid>
