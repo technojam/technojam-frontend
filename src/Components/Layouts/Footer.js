1faab890c30a82f3ff484eda0fb6d96295acdaed
ravi nandan saxena <44205807+ravisaxena23@users.noreply.github.com>
2019-09-01 04:03:18 +0530

    mobile view ready

--- src/Components/Layouts/Footer.js
+++ src/Components/Layouts/Footer.js
@@ -1,14 +1,13 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 
-import { AppBar, Toolbar, Button, IconButton, Hidden } from '@material-ui/core';
-import Paper from '@material-ui/core/Paper';
+
+import { AppBar, Toolbar, Hidden } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import CopyrightIcon from '@material-ui/icons/Copyright';
 
-const drawerWidth = 240;
+
 
 
 
@@ -18,13 +17,13 @@ export default props =>
             <Toolbar>
 
                 <Grid container direction="row" justify="flex-start" alignItems="flex-end">
-                    <CopyrightIcon />
-                    <Typography>
-                        Copyright 2019 Team TechnoJam
-                                        </Typography>
+                    
+                    <Typography variant="body2" style={{fontSize:'0.92rem'}}>
+                        Copyright <CopyrightIcon /> 2019 Team TechnoJam 
+                                        </Typography> 
                 </Grid>
                 <Grid container direction="row" >
-                    <div style={{ paddingLeft:'200px' }} >
+                    <div style={{ paddingLeft:'50px' }} >
                         <a className="btn btn-social-icon btn-twitter" href=" https://twitter.com/technojam_gu"><i className="fab fa-twitter fa-lg"style={{color:'#28aae1'}}></i></a>
                         <a className="btn btn-social-icon btn-facebook" href="facebook.com/technojam.scse "><i className="fab fa-facebook fa-lg"style={{color:'#3b5998'}}></i></a>
                         <a className="btn btn-social-icon btn-instagram" href="https://instagram.com/technojam_galgotias"><i className="fab fa-instagram fa-lg"style={{color:'#e41a2b'}}></i></a>
@@ -32,7 +31,8 @@ export default props =>
                         
                     </div>
                 </Grid>
-              
+                <Hidden smDown>
+           
                 <Grid container direction="row" >
                 <div style={{ paddingLeft:'100px' }}>
                     <Typography>
@@ -40,10 +40,7 @@ export default props =>
                      </Typography>
                 </div>
                 </Grid>
-               
-
-            </Toolbar>
+                </Hidden>
+               </Toolbar>
         </AppBar>
-
-
-    </div>
+</div>
