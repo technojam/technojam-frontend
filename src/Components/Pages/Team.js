d78dadf64bde78ac53d0de88b3e05ae44093716e
Aman Rawat <31853960+AmanRawat882@users.noreply.github.com>
2019-09-12 23:33:02 +0530

    Added Team Page

--- src/Components/Pages/Team.js
+++ src/Components/Pages/Team.js
@@ -1,14 +1,375 @@
-import React from 'react'
-/* @person who is assigned this page set your styles here. height 600 is just for layout.*/
-const style ={
-   
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Tooltip from '@material-ui/core/Tooltip';
+import Container from '@material-ui/core/Container';
+import Placeholder from '../../images/Placeholder.jpg';
+const useStyles = makeStyles(() => ({
+  root: {
+    flexGrow: 1,
+    marginTop:20,
+    marginBottom:20,
+  },  
+}));
+const circle={
+    width: 80,
+    borderRadius: 40,
 }
 
-function Team() {
-    return(
-        <div className="team" style={style}>
-            Team TechnoJam
-        </div>
-    );
+const tileData = [
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'Ravi ',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'Abhi',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'Aman',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'Ravi ',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'Abhi',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'Aman',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    }, {
+      img: Placeholder,
+      title: 'Image',
+      name: 'Ravi ',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'Abhi',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'Aman',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+    {
+      img: Placeholder,
+      title: 'Image',
+      name: 'name',
+    },
+   ];
+  
+export default function Team() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <h1 style={{color:"rgb(19, 20, 21)",textAlign:"center"}}>
+      Our Team
+      </h1>
+      <Container fixed >
+       <Grid container spacing={3} >
+        {tileData.map(tile => (
+        <Grid item xs={3} sm={2} lg={1}>
+            <Tooltip title={tile.name}>
+              <img src={tile.img} alt={tile.title} style={circle}/>        
+            </Tooltip>
+           
+        </Grid> 
+        ))}
+       </Grid>
+      </Container>
+      </div>
+  );
 }
-export default Team;
+
+/*{tile.name} To be deleted which is not in the tooltip after uploading pictures of every team members */
