1e15c58efae824da6dd892ec9f7085c9c24cb5a5
Aman Rawat <31853960+AmanRawat882@users.noreply.github.com>
2019-09-13 18:24:51 +0530

    Name Added

--- src/Components/Pages/Team.js
+++ src/Components/Pages/Team.js
@@ -17,336 +17,253 @@ const circle={
 }
 
 const tileData = [
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'Ravi ',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'Abhi',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'Aman',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'Ravi ',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'Abhi',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'Aman',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    }, {
-      img: Placeholder,
-      title: 'Image',
-      name: 'Ravi ',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'Abhi',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'Aman',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-    {
-      img: Placeholder,
-      title: 'Image',
-      name: 'name',
-    },
-   ];
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Shivam Mittal"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Kritika Saxena"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Anubhav Raj Singh"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Satyam Bharti"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Shivam Sharma"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Sakshi Gupta"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Darshan Baid"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Honey Sharma"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Rahul Chauhan"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Ankush Shrivastava"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Deepanshu Goyal"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Lokesh Tiwari"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Neham soni"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Prakhar Srivastava"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Naman Jain"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Anshika soni"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Ravi "
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Vidit Varshney"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Rohith Kandi"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "rahul godella"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Aman"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Rahul Singh "
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Vishal Kumar"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Shreyansh Deep"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Shashwat Singh"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "DANISH JAMAL"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "PRIYANSHU TRIVEDI"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Ayush Tiwari"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Astha Singh"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Abhimanyu Dwivedi"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Manas Khurana"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Vineeta chaudhary"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Saloni mehra"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Himanshu Singh"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Ayush Gupta"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Kushagra Singhal"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Ashwani Tripathi"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Neham soni"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Vineeta chaudhary"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Shashwat Singh"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Akshat akarsh"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Pradeep"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Anurag Verma"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "Mayank Jain "
+  },
+  {
+    img: Placeholder,
+    title: "image",
+    Name: "Saurabh kumar"
+  },
+  {
+    img: Placeholder,
+    title: "image",
+    Name: "Anil kumar yadav"
+  },
+  {
+    img: Placeholder,
+    title: "image",
+    Name: "Saloni mehra"
+  },
+  {
+    img: Placeholder,
+    title: "image",
+    Name: "Praveen singh"
+  },
+  {
+    img: Placeholder,
+    title: "Image",
+    Name: "ANKIT ANMOL SRIVASTAVA "
+  },
+ ];
+
   
 export default function Team() {
   const classes = useStyles();
@@ -360,7 +277,7 @@ export default function Team() {
        <Grid container spacing={3} >
         {tileData.map(tile => (
         <Grid item xs={3} sm={2} lg={1}>
-            <Tooltip title={tile.name}>
+            <Tooltip title={tile.Name}>
               <img src={tile.img} alt={tile.title} style={circle}/>        
             </Tooltip>
            
