80dc2f6159089b5715dcc34c3542dae6c54ee97e
ravi nandan saxena <44205807+ravisaxena23@users.noreply.github.com>
2019-09-14 21:27:19 +0530

    Update Contactus.js

--- src/Components/Pages/Contactus.js
+++ src/Components/Pages/Contactus.js
@@ -14,7 +14,7 @@ function Contactus() {
         <div className="contactus" >
             <br></br>
             <Hidden smUp>
-                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeaesLCX8qedDLQVu3qLrP9xQX-NpaN3ZUzKDwMSJjf59vRTg/viewform?embedded=true" width="auto" height="700px" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeaesLCX8qedDLQVu3qLrP9xQX-NpaN3ZUzKDwMSJjf59vRTg/viewform?embedded=true" width="300px" height="700px" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
             </Hidden>
            
             <Hidden smDown>
