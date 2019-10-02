0ea3fbbcbc371b9cb2aa7171d6c6bf53c71ef867
Alex Anderson <alexandersonbm@hotmail.com>
2019-10-02 14:19:59 -0300

    :lipstick: Fix footer on bottom position

--- src/App.js
+++ src/App.js
@@ -4,26 +4,29 @@ import { TopMenu, Footer} from './Components/Layouts'
 import Maincontent from './Components/Maincontent'
 
 var sectionStyle = {
-  width: "100%",
-  height: "auto",
-  backgroundImage: "url(https://tj-static.s3.ap-south-1.amazonaws.com/etc/Capture.png)",
-  backgroundRepeat: "repeat-y",
-  backgroundPositionX: "center"
+    width: "100%",
+    height: "auto",
+    backgroundImage: "url(https://tj-static.s3.ap-south-1.amazonaws.com/etc/Capture.png)",
+    backgroundRepeat: "repeat-y",
+    backgroundPositionX: "center"
 
 };
 
 function App() {
-  return (
-    <div className="App" style={sectionStyle}>
-      <Fragment>
-      <TopMenu />
-     
-      <Maincontent />
-      <Footer/>
+    return (
+        <Fragment>
+            <div className="App" style={sectionStyle}>
+                <Fragment>
+                    <TopMenu />
+
+                    <Maincontent />
+
+                </Fragment>
+
+            </div>
+            <Footer />
         </Fragment>
- </div>
-    
-  );
+    );
 }
 
 export default App;
