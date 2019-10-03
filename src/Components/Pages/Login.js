47f925f10d888b14aeea840adaf4c7f1fb74a36b
Denny Pradipta <denny.aditya.p@gmail.com>
2019-10-03 11:39:48 +0700

    Fix card shadow and JSX indentations

--- src/Components/Pages/Login.js
+++ src/Components/Pages/Login.js
@@ -1,14 +1,12 @@
-import React from 'react'
+import React from 'react';
 /* @person who is assigned this page set your styles here. height 600 is just for layout.*/
-const style ={
-   
-}
+const style = {};
 
 function Login() {
-    return(
-        <div className="mentorship" style={style}>
-            Events
-        </div>
-    );
+	return (
+		<div className="mentorship" style={style}>
+			Events
+		</div>
+	);
 }
 export default Login;
--- src/Components/Pages/Resources.js
+++ src/Components/Pages/Resources.js
@@ -1,19 +1,18 @@
-import React from 'react'
+import React from 'react';
 import Container from '@material-ui/core/Container';
 /* @person who is assigned this page set your styles here. height 600 is just for layout.*/
-const style ={
-    minHeight:"600px",
-    padding:"100px"
-}
+const style = {
+	minHeight: '600px',
+	padding: '100px'
+};
 
 function Resources() {
-    return(
-        <Container maxWidth="sm">
-                    <div className="mentorship" style={style}>
-            Resources coming soon in version 2.0
-        </div>
-        </Container>
-
-    );
+	return (
+		<Container maxWidth="sm">
+			<div className="mentorship" style={style}>
+				Resources coming soon in version 2.0
+			</div>
+		</Container>
+	);
 }
 export default Resources;
