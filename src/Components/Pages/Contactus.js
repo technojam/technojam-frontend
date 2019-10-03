d0736c83176431f434ae02db7f27491779555154
Terence Chambers <terencechambers91@gmail.com>
2019-10-03 11:02:43 -0400

    Contact Form Update attempt

--- src/Components/Pages/Contactus.js
+++ src/Components/Pages/Contactus.js
@@ -1,182 +1,201 @@
-
 import React, { Component } from 'react';
 
 //material ui component
 import Container from '@material-ui/core/Container';
-import { Typography, Hidden, Grid, Button, CardHeader, CardContent } from '@material-ui/core';
+import {
+  Typography,
+  Hidden,
+  Grid,
+  Button,
+  CardHeader,
+  CardContent
+} from '@material-ui/core';
 import Card from '@material-ui/core/Card';
 import TextField from '@material-ui/core/TextField';
 import clsx from 'clsx';
 import { FormGroup } from '@material-ui/core';
 import Divider from '@material-ui/core/Divider';
 import {
-	TwitterTimelineEmbed,
-	TwitterShareButton,
-	TwitterFollowButton,
-	TwitterHashtagButton,
-	TwitterMentionButton,
-	TwitterTweetEmbed,
-	TwitterMomentShare,
-	TwitterDMButton,
-	TwitterVideoEmbed,
-	TwitterOnAirButton
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterFollowButton,
+  TwitterHashtagButton,
+  TwitterMentionButton,
+  TwitterTweetEmbed,
+  TwitterMomentShare,
+  TwitterDMButton,
+  TwitterVideoEmbed,
+  TwitterOnAirButton
 } from 'react-twitter-embed';
 
 var sectionStyle = {
-	width: '100%',
-	height: '85vh',
-	backgroundRepeat: 'no-repeat',
-	marginTop: '100px',
-	backgroundPositionX: 'center',
-	Height: '100%'
+  width: '100%',
+  height: '85vh',
+  backgroundRepeat: 'no-repeat',
+  marginTop: '100px',
+  backgroundPositionX: 'center',
+  Height: '100%'
 };
 const style = {
-	form: {
-		padding: '20px',
-		text: {
-			appearance: 'none',
-			width: '100%',
+  form: {
+    padding: '20px',
+    text: {
+      appearance: 'none',
+      width: '100%',
 
-			height: '35px',
+      height: '35px',
 
-			borderRadius: '5px',
-			outline: 'none',
-			border: 'none',
-			background: '#e8ebed',
-			color: '#576366',
-			fontSize: '14px'
-		}
-	}
+      borderRadius: '5px',
+      outline: 'none',
+      border: 'none',
+      background: '#e8ebed',
+      color: '#576366',
+      fontSize: '14px'
+    }
+  }
 };
 
 class Mentorship extends Component {
-	render() {
-		return (
-			<Container maxWidth="xl ">
-				<div style={sectionStyle}>
-					<Grid container direction="row" justify="flex-start" alignItems="flex-start" spacing={6}>
-						<Grid direction="row" xs={12} sm={12} md={6}>
-							<Card className="card--shadow" style={{ borderRadius: '10px', marginTop: '23px' }}>
-								<div style={{ backgroundColor: '#24292E' }}>
-									<br />
-									<CardContent>
-										<Hidden smUp>
-											<Typography
-												variant="h4"
-												style={{
-													fontFamily: 'Roboto',
-													letterSpacing: '1px',
-													lineHeight: '28px',
-													wordSpacing: '5px',
-													textAlign: 'center',
-													color: 'white'
-												}}
-											>
-												Get in touch
-											</Typography>
-										</Hidden>
+  render() {
+    return (
+      <Container maxWidth='xl '>
+        <div style={sectionStyle}>
+          <Grid
+            container
+            direction='row'
+            justify='flex-start'
+            alignItems='flex-start'
+            spacing={6}
+          >
+            <Grid direction='row' xs={12} sm={12} md={6}>
+              <Card
+                className='card--shadow'
+                style={{ borderRadius: '10px', marginTop: '23px' }}
+              >
+                <div style={{ backgroundColor: '#24292E' }}>
+                  <br />
+                  <CardContent>
+                    <Hidden smUp>
+                      <Typography
+                        variant='h4'
+                        style={{
+                          fontFamily: 'Roboto',
+                          letterSpacing: '1px',
+                          lineHeight: '28px',
+                          wordSpacing: '5px',
+                          textAlign: 'center',
+                          color: 'white'
+                        }}
+                      >
+                        Get in touch
+                      </Typography>
+                    </Hidden>
 
-										<Hidden mdDown>
-											<Typography
-												variant="h2"
-												style={{
-													fontFamily: 'Roboto',
-													letterSpacing: '1px',
-													lineHeight: '28px',
-													wordSpacing: '5px',
-													textAlign: 'center',
-													color: 'white'
-												}}
-											>
-												Get in touch
-											</Typography>
-										</Hidden>
-									</CardContent>
-								</div>
-								<Divider />
-								<br />
-								<div style={style.form}>
-									<form autoComplete="on">
-										<Grid container spacing={3}>
-											<Grid item xs={6}>
-												<FormGroup>
-													<label>First-Name</label>
-													<input
-														style={style.form.text}
-														type="text"
-														name="Firstname"
-														onChange={this.handleChange}
-													/>
-												</FormGroup>
-											</Grid>
-											<Grid item xs={6}>
-												<FormGroup>
-													<label>Last-Name</label>
-													<input
-														style={style.form.text}
-														type="text"
-														name="Lastname"
-														onChange={this.handleChange}
-													/>
-												</FormGroup>
-											</Grid>
-										</Grid>
-										<FormGroup>
-											<label>Email</label>
-											<input
-												style={style.form.text}
-												type="email"
-												name="Email"
-												onChange={this.handleChange}
-											/>
-										</FormGroup>
-										<FormGroup>
-											<label>Contact Number</label>
-											<input
-												style={style.form.text}
-												type="tel"
-												name="Mobile"
-												onChange={this.handleChange}
-											/>
-										</FormGroup>
-										<FormGroup>
-											<label>Query</label>
-											<input
-												style={style.form.text}
-												type="textarea"
-												name="Reason"
-												onChange={this.handleChange}
-											/>
-										</FormGroup>
-										<br />
-										<Typography style={{ textAlign: 'center', fontFamily: 'Roboto' }}>
-											<Button
-												variant="contained"
-												style={{ backgroundColor: ' #e03030', color: 'white' }}
-											>
-												Submit <a href="#" target="_blank" />
-											</Button>
-										</Typography>
-									</form>
-								</div>
-							</Card>
-						</Grid>
-						<Grid item xs={12} sm={6} md={6}>
-							<Card style={{ borderRadius: '10px', fontSize: '14px' }}>
-								<TwitterTimelineEmbed
-									sourceType="profile"
-									screenName="technojam_gu"
-									options={{ height: 440 }}
-									size="20px"
-								/>
-							</Card>
-						</Grid>
-					</Grid>
-					<br />
-				</div>
-			</Container>
-		);
-	}
+                    <Hidden mdDown>
+                      <div className='banner-text'>
+                        <Typography
+                          variant='h2'
+                          style={{
+                            fontFamily: 'Roboto',
+                            letterSpacing: '1px',
+                            lineHeight: '28px',
+                            wordSpacing: '5px',
+                            textAlign: 'center',
+                            color: '#F08080'
+                          }}
+                        >
+                          Contact Me
+                        </Typography>
+                      </div>
+                    </Hidden>
+                  </CardContent>
+                </div>
+                <Divider />
+                <br />
+                <div style={style.form}>
+                  <form autoComplete='on'>
+                    <Grid container spacing={3}>
+                      <Grid item xs={6}>
+                        <FormGroup>
+                          <label>First-Name</label>
+                          <input
+                            style={style.form.text}
+                            type='text'
+                            name='Firstname'
+                            onChange={this.handleChange}
+                          />
+                        </FormGroup>
+                      </Grid>
+                      <Grid item xs={6}>
+                        <FormGroup>
+                          <label>Last-Name</label>
+                          <input
+                            style={style.form.text}
+                            type='text'
+                            name='Lastname'
+                            onChange={this.handleChange}
+                          />
+                        </FormGroup>
+                      </Grid>
+                    </Grid>
+                    <FormGroup>
+                      <label>Email</label>
+                      <input
+                        style={style.form.text}
+                        type='email'
+                        name='Email'
+                        onChange={this.handleChange}
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <label>Contact Number</label>
+                      <input
+                        style={style.form.text}
+                        type='tel'
+                        name='Mobile'
+                        onChange={this.handleChange}
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <label>Details</label>
+                      <input
+                        style={style.form.text}
+                        type='textarea'
+                        name='Reason'
+                        onChange={this.handleChange}
+                      />
+                    </FormGroup>
+                    <br />
+                    <Typography
+                      style={{ textAlign: 'center', fontFamily: 'Roboto' }}
+                    >
+                      <Button
+                        variant='contained'
+                        style={{ backgroundColor: ' #FFFF99', color: 'black' }}
+                      >
+                        Submit <a href='#' target='_blank' />
+                      </Button>
+                    </Typography>
+                  </form>
+                </div>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={6} md={6}>
+              <Card style={{ borderRadius: '10px', fontSize: '14px' }}>
+                <TwitterTimelineEmbed
+                  sourceType='profile'
+                  screenName='technojam_gu'
+                  options={{ height: 440 }}
+                  size='20px'
+                />
+              </Card>
+            </Grid>
+          </Grid>
+          <br />
+        </div>
+      </Container>
+    );
+  }
 }
 
 export default Mentorship;
