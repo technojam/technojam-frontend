210a90fc7294b4b27deeadf69c870ec24f968156
Branislav Osif <branislav.osif@student.tuke.sk>
2019-10-08 10:37:33 +0200

    Fixed scroll up button. No need for 'react-floating-action-button' dependency.

--- src/App.js
+++ src/App.js
@@ -1,49 +1,48 @@
-
 import React, {Fragment, useState} from "react";
-import {TopMenu, Footer} from "./Components/Layouts";
+import {Footer, TopMenu} from "./Components/Layouts";
 import Maincontent from "./Components/Maincontent";
-import { Container, Button, Link } from 'react-floating-action-button';
+import {Container} from 'react-floating-action-button';
+import {Fab, Icon} from "@material-ui/core"
 
 const sectionStyle = {
-   backgroundImage: 'url(https://tj-static.s3.ap-south-1.amazonaws.com/etc/Capture.png)',
-   backgroundRepeat: 'repeat',
-   backgroundPositionX: 'center',
-   position: 'absolute',
-   width: '100%',
-   top: '0'
+    backgroundImage: 'url(https://tj-static.s3.ap-south-1.amazonaws.com/etc/Capture.png)',
+    backgroundRepeat: 'repeat',
+    backgroundPositionX: 'center',
+    position: 'absolute',
+    width: '100%',
+    top: '0'
 };
 
 function App() {
-  const [panel, setPanel] = useState(false);
+    const [panel, setPanel] = useState(false);
 
-  function toggleButton() {
-    if (!panel) setPanel(true);
-    else setPanel(false);
+    function toggleButton() {
+        if (!panel) setPanel(true);
+        else setPanel(false);
 
-    if (panel === false) {
-      document.getElementById("ToggleButton").style.background = "#161625";
+        if (panel === false) {
+            document.getElementById("ToggleButton").style.background = "#161625";
+        }
     }
-  }
 
-  return (
-    <div className="App" style={sectionStyle}>
-      <Fragment>
-        <TopMenu toggleButton={toggleButton} />
-         <Maincontent />
-         <br />
-         <br />
-         <br />
-         <Footer />
-      </Fragment>
-              <Container>
-            <Button
-                tooltip="The big plus button!"
-                icon="fas fa-chevron-up"
-                rotate={false}
-                onClick={() => window.scrollTo(0,0)} />
-        </Container>
-   </div>
-);
-  }
+    return (
+        <div className="App" style={sectionStyle}>
+            <Fragment>
+                <TopMenu toggleButton={toggleButton}/>
+                <Maincontent/>
+                <br/>
+                <br/>
+                <br/>
+                <Footer/>
+            </Fragment>
+            <Container>
+                <Fab color="secondary" onClick={() => window.scrollTo(0, 0)}>
+                    <Icon style={{height: "auto"}} className="fas fa-chevron-up"/>
+                </Fab>
+            </Container>
+        </div>
+    );
+}
 
 export default App;
+
