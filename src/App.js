4a4855cbadea660c88566fb28db58fa90b9520e9
Honey Sharma <honey.singhroi@gmail.com>
2019-10-08 11:47:37 +0530

    login register logout connected to backend

--- src/App.js
+++ src/App.js
@@ -1,42 +1,44 @@
-
-import React, {Fragment, useState} from "react";
-import {TopMenu, Footer} from "./Components/Layouts";
-import Maincontent from "./Components/Maincontent";
-
-
+import React, { Fragment, useState, useContext } from 'react';
+import { TopMenu, Footer } from './Components/Layouts';
+import Maincontent from './Components/Maincontent';
+import AuthContext from './context/auth/AuthState';
+import AlertContext from './context/alert/AlertState';
 const sectionStyle = {
-   backgroundImage: 'url(https://tj-static.s3.ap-south-1.amazonaws.com/etc/Capture.png)',
-   backgroundRepeat: 'repeat',
-   backgroundPositionX: 'center',
-   position: 'absolute',
-   width: '100%',
-   top: '0'
+	backgroundImage:
+		'url(https://tj-static.s3.ap-south-1.amazonaws.com/etc/Capture.png)',
+	backgroundRepeat: 'repeat',
+	backgroundPositionX: 'center',
+	position: 'absolute',
+	width: '100%',
+	top: '0'
 };
 
 function App() {
-  const [panel, setPanel] = useState(false);
+	const [panel, setPanel] = useState(false);
 
-  function toggleButton() {
-    if (!panel) setPanel(true);
-    else setPanel(false);
+	function toggleButton() {
+		if (!panel) setPanel(true);
+		else setPanel(false);
 
-    if (panel === false) {
-      document.getElementById("ToggleButton").style.background = "#161625";
-    }
-  }
+		if (panel === false) {
+			document.getElementById('ToggleButton').style.background = '#161625';
+		}
+	}
 
-  return (
-    <div className="App" style={sectionStyle}>
-      <Fragment>
-        <TopMenu toggleButton={toggleButton} />
-         <Maincontent />
-         <br />
-         <br />
-         <br />
-         <Footer />
-      </Fragment>
-   </div>
-);
-  }
+	return (
+		<div className='App' style={sectionStyle}>
+			<AuthContext>
+				<AlertContext>
+					<TopMenu toggleButton={toggleButton} />
+					<Maincontent />
+					<br />
+					<br />
+					<br />
+					<Footer />
+				</AlertContext>
+			</AuthContext>
+		</div>
+	);
+}
 
 export default App;
