dd8d912d2b62a980593dd58102c5bbf9ea2d27df
ravi nandan saxena <44205807+ravisaxena23@users.noreply.github.com>
2019-10-12 23:33:12 +0530

    old event page it looks odd

--- src/Components/Pages/Alumni.js
+++ src/Components/Pages/Alumni.js
@@ -17,7 +17,6 @@ import {Link} from "react-router-dom";
  */
 
 
-
 const Alumni = () => {
    //I'm assuming these are just temporarily static; if not, remove from state and just hard code them
     return (
@@ -64,105 +63,6 @@ const Alumni = () => {
               alignItems="flex-start"
             >
                             {ALUMNI.map(achie => (
-
-                },
-                {
-                    id: 4,
-                    title: "Kaustubh",
-                    Google: "kausr2595@gmail.com",
-                    Linkedin: "https://www.linkedin.com/in/kausr2595/",
-                    Image: "https://tj-static.s3.ap-south-1.amazonaws.com/etc/Kaustubh+Rai.JPG",
-                },
-                {
-                    id: 5,
-                    title: "Aditya Kumar ",
-                    Google: "Aditya12.official@gmail.com ",
-                    Linkedin: "#",
-                    Image: "https://tj-static.s3.ap-south-1.amazonaws.com/etc/Aditya+kumar.jpg",
-                },
-                {
-                    id: 6,
-                    title: "Shivashish Ratnam",
-                    Google: "shivashishratnam@gmail.com",
-                    Linkedin: "https://www.linkedin.com/in/shivashish-ratnam/",
-                    Image: "https://tj-static.s3.ap-south-1.amazonaws.com/etc/Shivashish+Ratnam.jpg",
-
-                },
-                {
-                    id: 7,
-                    title: "Satya Tripathi",
-                    Google: "satya06.official@gmail.com",
-                    Linkedin: "#",
-                    Image: "https://tj-static.s3.ap-south-1.amazonaws.com/etc/Satya+Tripathi.jpg",
-
-                },
-                {
-                    id: 8,
-                    title: "shashank sharma",
-                    Google: "shashank@thelattice.in",
-                    Linkedin: "https://www.linkedin.com/in/storytellerr/",
-                    Image: "https://tj-static.s3.ap-south-1.amazonaws.com/etc/shashank+sharma.jpg",
-
-                },
-                {
-                    id: 9,
-                    title: "Shaurya Chauhan",
-                    Google: "scshaurya@gmail.com",
-                    Linkedin: "https://www.linkedin.com/in/shauryachauhan/",
-                    Image: "https://tj-static.s3.ap-south-1.amazonaws.com/etc/shaurya+chauhan.jpg",
-                }
-            ]
-        };
-    }
-
-    render() {
-        const achie = this.state.achie;
-
-        console.log(achie);
-        return (
-            <Container maxWidth="lg">
-                <div
-                    style={{
-                        marginTop: "80px",
-                        marginBottom: "20px",
-                        maxWidth: "sm"
-                    }}
-                >
-                    <div>
-                        <br />{" "}
-                        <Typography
-                            variant="h3"
-                            style={{
-                                fontFamily: "Roboto",
-                                letterSpacing: "1px",
-                                lineHeight: "28px",
-                                wordSpacing: "5px",
-                                textAlign: "center",
-                                color: "#073fff"
-                            }}
-                        >
-                            Our Alumni{" "}
-                        </Typography>{" "}
-                        <hr
-                            style={{
-                                maxWidth: "120px",
-                                borderWidth: "0.6px",
-                                opacity: "1",
-                                marginTop: "25px",
-                                marginBottom: "25px",
-                                borderColor: "#007FFF"
-                            }}
-                        ></hr>{" "}
-                        <br />
-                        <Grid
-                            container
-                            spacing={3}
-                            direction="row"
-                            justify="flex-start"
-                            alignItems="flex-start"
-                        >
-                            {achie.map(achie => (
-
                                 <Grid item xs={6} sm={4} md={3}>
                                     <Card
                                         className="card--shadow view_card"
