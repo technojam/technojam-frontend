3d54df7405873e26b6de30d11d9333ed73014a55
Lakshya Bansal <39180629+lakshya-20@users.noreply.github.com>
2020-09-18 01:15:23 +0530

    converted liveEvent class component to functional component

--- src/Components/Layouts/liveEvents.js
+++ src/Components/Layouts/liveEvents.js
@@ -1,114 +1,5 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
 import { MoonLoader } from 'react-spinners';
-
-const defaultEventsList = [
-	{
-		id: 1,
-		title: 'Dextrix 2.0',
-		feedback: '#',
-		date: '233 November, 2019',
-		venue: ' GU',
-		todo: '#',
-		description:
-			"24 hours hackthon for all."
-	},
-];
-
-const liveEventStyle = {
-	position: 'fixed',
-	right: '0',
-	top: '100px',
-	display: 'flex',
-	zIndex: '999'
-};
-
-export default class liveEvents extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			open: false,
-			list: [],
-			showList: false,
-			dataFetched: false
-		};
-		this.toggleOpen = this.toggleOpen.bind(this);
-		this.toggleShowList = this.toggleShowList.bind(this);
-	}
-
-	componentDidMount() {
-		//fetch the event list data from server and set to response to list array
-
-		//the below code ,is simulating a fetching data from server , remove it in actual code
-		setTimeout(() => {
-			this.setState({
-				...this.state,
-				list: defaultEventsList, //using hardcoded values,
-				dataFetched: true
-			});
-		}, 2000);
-	}
-
-	toggleOpen() {
-		this.setState({
-			...this.state,
-			open: !this.state.open
-		});
-	}
-
-	toggleShowList = () => {
-		this.setState({
-			...this.state,
-			showList: !this.state.showList
-		});
-	};
-
-	render() {
-		return (
-			<div style={liveEventStyle}>
-				<EventLabel
-					dataFetched={this.state.dataFetched}
-					toggleOpen={this.toggleOpen}
-					count={this.state.list.length}
-				/>
-				<EventContent
-					dataFetched={this.state.dataFetched}
-					toggleShowlist={this.toggleShowList}
-					showlist={this.state.showList}
-					open={this.state.open}
-					list={this.state.list}
-				/>
-			</div>
-		);
-	}
-}
-
-const nameStyle = {
-	marginTop: '62px',
-	display: 'inline-block',
-	writingMode: 'vertical-rl',
-	background: '#23292e',
-	color: '#fff',
-	padding: '10px',
-	borderRadius: '3px',
-	cursor: 'pointer',
-	Height: '82px',
-	width: '40px'
-};
-
-const countStyle = {
-	marginTop: '62px',
-	position: 'absolute',
-	top: '-10px',
-	left: '-15px',
-	background: '#e53935',
-	width: '20px',
-	height: '20px',
-	textAlign: 'center',
-	writingMode: 'horizontal-tb',
-	borderRadius: '3px',
-	boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)'
-};
 
 const EventLabel = props => {
 	return (
@@ -117,34 +8,6 @@ const EventLabel = props => {
 			<span style={countStyle}>{props.dataFetched ? props.count : null}</span>
 		</div>
 	);
-};
-
-const eventContentStyle = {
-	marginTop: '62px',
-	display: 'inline-block',
-	float: 'right',
-	transition: 'width 1s',
-	border: '1px solid rgb(238, 238, 238)',
-	background: '#fff',
-	alignItems: 'center',
-	textAlign: 'center',
-	maxHeight: '232px',
-	overflowX: 'hidden'
-};
-
-const eventListsStyle = {
-	listStyleType: 'none',
-	background: '#fff',
-	fontSize: '14px',
-	borderBottom: '1px solid #EEEEEE',
-	color: 'rgba(0, 0, 0, 0.54)',
-	display: 'flex',
-	padding: '0.5em'
-};
-
-const font10Margin0 = {
-	fontSize: '10px',
-	margin: '0'
 };
 
 const EventContent = props => {
@@ -173,8 +36,8 @@ const EventContent = props => {
 							>
 								{d.title}
 							</a>
-							<p style={font10Margin0}>{d.date}</p>
-							<p style={font10Margin0}>{d.venue}</p>
+							<p style={{font:'10',Margin:'0'}}>{d.date}</p>
+							<p style={{font:'10',Margin:'0'}}>{d.venue}</p>
 						</div>
 					</li>
 				))}
@@ -188,4 +51,123 @@ const EventContent = props => {
 			) : null}
 		</div>
 	);
+};
+
+function LiveEvents() {
+	const [open, setOpen] = useState(false);
+	const [showList, setShowList] = useState(false);
+	const [dataFetched, setDataFetched] = useState(false);
+	const [list, setList] = useState([]);
+
+	const event_data=[
+		{
+			id: 1,
+			title: 'Dextrix 2.0',
+			feedback: '#',
+			date: '233 November, 2019',
+			venue: ' GU',
+			todo: '#',
+			description:
+				"24 hours hackthon for all."
+		},
+	];
+	useEffect(()=>{
+		setTimeout(()=>{
+			setList(event_data);
+			setDataFetched(true);
+		})
+	});
+
+	const toggleOpen=()=>{
+		setOpen(!open)
+	}
+
+	const toggleShowList=()=>{
+		setShowList(!showList);
+	}
+
+	return (
+		<div style={liveEventStyle}>
+			<EventLabel
+				dataFetched={dataFetched}
+				toggleOpen={toggleOpen}
+				count={list.length}
+			/>
+			<EventContent
+				dataFetched={dataFetched}
+				toggleShowlist={toggleShowList}
+				showlist={showList}
+				open={open}
+				list={list}
+			/>
+		</div>
+	);
+}
+
+export default LiveEvents;
+
+
+
+
+
+
+
+
+
+const liveEventStyle = {
+	position: 'fixed',
+	right: '0',
+	top: '100px',
+	display: 'flex',
+	zIndex: '999'
+};
+
+const nameStyle = {
+	marginTop: '62px',
+	display: 'inline-block',
+	writingMode: 'vertical-rl',
+	background: '#23292e',
+	color: '#fff',
+	padding: '10px',
+	borderRadius: '3px',
+	cursor: 'pointer',
+	Height: '82px',
+	width: '40px'
+};
+
+const countStyle = {
+	marginTop: '62px',
+	position: 'absolute',
+	top: '-10px',
+	left: '-15px',
+	background: '#e53935',
+	width: '20px',
+	height: '20px',
+	textAlign: 'center',
+	writingMode: 'horizontal-tb',
+	borderRadius: '3px',
+	boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)'
+};
+
+const eventContentStyle = {
+	marginTop: '62px',
+	display: 'inline-block',
+	float: 'right',
+	transition: 'width 1s',
+	border: '1px solid rgb(238, 238, 238)',
+	background: '#fff',
+	alignItems: 'center',
+	textAlign: 'center',
+	maxHeight: '232px',
+	overflowX: 'hidden'
+};
+
+const eventListsStyle = {
+	listStyleType: 'none',
+	background: '#fff',
+	fontSize: '14px',
+	borderBottom: '1px solid #EEEEEE',
+	color: 'rgba(0, 0, 0, 0.54)',
+	display: 'flex',
+	padding: '0.5em'
 };
