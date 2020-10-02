cdf6f633019af6cf413dfd985756983e8ed60e82
NiketanG <nikegulekar@gmail.com>
2020-10-02 12:38:06 +0530

    :bug: Fixed Light-Dark Mode, :art: Added Prettier Config
    Added ThemeContext for easier access and toggling of theme. Uses React Context.
    Added Prettier config, and updated Eslint to work with Prettier

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import './App.css';
-import React, { Fragment, useState, useContext } from 'react';
-import { TopMenu, Footer } from './Components/Layouts';
-import Maincontent from './Components/Maincontent';
-import AuthContext from './context/auth/AuthState';
-import AlertContext from './context/alert/AlertState';
-import EventContext from './context/event/eventState';
+import "./App.css";
+import React, { useState } from "react";
+import { TopMenu, Footer } from "./Components/Layouts";
+import Maincontent from "./Components/Maincontent";
+import AuthContext from "./context/auth/AuthState";
+import AlertContext from "./context/alert/AlertState";
+import EventContext from "./context/event/eventState";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
-import { Hidden, Fab, Icon } from '@material-ui/core';
+import { Hidden, Fab, Icon } from "@material-ui/core";
+import { ThemeContextProvider } from "./util/themeContext";
 
 const sectionStyle = {
 	backgroundImage:
-		'url(https://tj-static.s3.ap-south-1.amazonaws.com/etc/Untitled-1.png)',
-	backgroundRepeat: 'repeat',
-	backgroundPositionX: 'center',
-	position: 'absolute',
-	width: '100%',
-	top: '0'
+		"url(https://tj-static.s3.ap-south-1.amazonaws.com/etc/Untitled-1.png)",
+	backgroundRepeat: "repeat",
+	backgroundPositionX: "center",
+	position: "absolute",
+	width: "100%",
+	top: "0",
 };
 
 const fabPosition = {
-	bottom: '10vh',
-	position: 'fixed',
-	margin: '1em',
-	right: '8vw',
-	opacity: '0.9'
+	bottom: "10vh",
+	position: "fixed",
+	margin: "1em",
+	right: "8vw",
+	opacity: "0.9",
 };
 
 function App() {
@@ -35,38 +38,45 @@ function App() {
 		else setPanel(false);
 
 		if (panel === false) {
-			document.getElementById('ToggleButton').style.background = '#161625';
+			document.getElementById("ToggleButton").style.background =
+				"#161625";
 		}
 	}
 
 	return (
-		<div className='dark-mode'>
-			<div className='App ' style={sectionStyle}>
-				<AuthContext>
-					<EventContext>
-						<AlertContext>
-							<TopMenu toggleButton={toggleButton} />
-							<Maincontent />
-							<br />
-							<br />
-							<br />
-							<Footer />
-						</AlertContext>
-					</EventContext>
-				</AuthContext>
-				<Hidden smUp>
-					<div style={fabPosition}>
-						<Fab color='secondary' onClick={() => window.scrollTo(0, 0)}>
-							<Icon
-								fontSize='inherit'
-								style={{ height: 'auto' }}
-								className='fas fa-chevron-up'
-							/>
-						</Fab>
-					</div>
-				</Hidden>
+		<ThemeContextProvider>
+			<CssBaseline />
+			<div className="dark-mode">
+				<div className="App " style={sectionStyle}>
+					<AuthContext>
+						<EventContext>
+							<AlertContext>
+								<TopMenu toggleButton={toggleButton} />
+								<Maincontent />
+								<br />
+								<br />
+								<br />
+								<Footer />
+							</AlertContext>
+						</EventContext>
+					</AuthContext>
+					<Hidden smUp>
+						<div style={fabPosition}>
+							<Fab
+								color="secondary"
+								onClick={() => window.scrollTo(0, 0)}
+							>
+								<Icon
+									fontSize="inherit"
+									style={{ height: "auto" }}
+									className="fas fa-chevron-up"
+								/>
+							</Fab>
+						</div>
+					</Hidden>
+				</div>
 			</div>
-		</div>
+		</ThemeContextProvider>
 	);
 }
 export default App;
